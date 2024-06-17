1fa79a270ed4ebda69af2005c16c94000270e283
bxbyte <lucas.maillet.app@gmail.com>
2024-06-17 18:39:32 +0200

    Fixed missing inheritance

--- src/main/java/com/musigma/controllers/CalendarController.java
+++ src/main/java/com/musigma/controllers/CalendarController.java
@@ -1,6 +1,6 @@
 package com.musigma.controllers;
 
-public class CalendarController {
+public class CalendarController extends WorkspaceController {
     public static WorkspaceController.WorkspaceRegister REGISTER = new WorkspaceController.WorkspaceRegister(
             "Calendar",
             "/com/musigma/images/icons/calendar.png",
--- src/main/java/com/musigma/controllers/TicketController.java
+++ src/main/java/com/musigma/controllers/TicketController.java
@@ -1,6 +1,6 @@
 package com.musigma.controllers;
 
-public class TicketController {
+public class TicketController extends WorkspaceController {
     public static WorkspaceController.WorkspaceRegister REGISTER = new WorkspaceController.WorkspaceRegister(
             "Ticket",
             "/com/musigma/images/icons/ticket.png",
