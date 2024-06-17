26fd458d7b4899279d94155c49365c2e3d095010
Damien ARAVENA BRAVO <damien.aravena-bravo@etu.univ-grenoble-alpes.fr>
2024-06-17 16:02:43 +0200

    Edit: main-view and MainController

--- src/main/java/com/musigma/Main.java
+++ src/main/java/com/musigma/Main.java
@@ -1,5 +1,6 @@
 package com.musigma;
 
+import com.musigma.controllers.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,14 +11,20 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/musigma/views/main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/musigma/views/main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.initStyle(javafx.stage.StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
+
+        ((MainController) fxmlLoader.getController()).addWorkspace(
+            "test",
+            getClass().getResource("/com/musigma/images/home.png").getPath(),
+            getClass().getResource("/com/musigma/views/test.fxml").getPath()
+        );
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/com/musigma/controllers/MainController.java
+++ src/main/java/com/musigma/controllers/MainController.java
@@ -1,14 +1,89 @@
 package com.musigma.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    Button minimizeButton, maximizeButton, closeButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    VBox pageMenu;
+
+    @FXML
+    VBox workspace;
+
+    @FXML
+    public void initialize() {
+        minimizeButton.setOnAction(this::handleButtonAction);
+        maximizeButton.setOnAction(this::handleButtonAction);
+        closeButton.setOnAction(this::handleButtonAction);
+
+        addHoverEffect(minimizeButton, "lightgrey");
+        addHoverEffect(maximizeButton, "lightgrey");
+        addHoverEffect(closeButton, "red");
+    }
+
+    private void handleButtonAction(ActionEvent event) {
+        Button button = (Button) event.getSource();
+        Stage window = (Stage) button.getScene().getWindow();
+        switch (button.getId()) {
+            case "minimizeButton":
+                window.setIconified(true);
+                System.out.println("Minimize button clicked");
+                break;
+            case "maximizeButton":
+                window.setMaximized(!window.isMaximized());
+                System.out.println("Maximize button clicked");
+                break;
+            case "closeButton":
+                window.close();
+                System.out.println("Close button clicked");
+                break;
+        }
+    }
+
+    private void addHoverEffect(Button button, String color) {
+        button.setOnMouseEntered(e -> button.setStyle("-fx-background-color: " + color + ";"));
+        button.setOnMouseExited(e -> button.setStyle("-fx-background-color: transparent;"));
+    }
+
+    public void addWorkspace(String pageName, String pathToIcon, String pathToFXML) {
+        System.out.println(pageName);
+        System.out.println(pathToIcon);
+        System.out.println(pathToFXML);
+        VBox pageButtonBox = new VBox();
+        pageButtonBox.setAlignment(Pos.CENTER);
+//        ImageView icon = new ImageView();
+//        icon.setImage(new Image("@../images/home.png"));
+//        icon.setImage(new Image(pathToIcon));
+        Label pageLabel = new Label();
+        pageLabel.setText(pageName);
+        pageButtonBox.getChildren().addAll(pageLabel);
+//        pageButtonBox.getChildren().addAll(icon, pageLabel);
+        pageButtonBox.setOnMouseClicked(ev -> {
+            try {
+                setWorkspace(pathToFXML);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        pageMenu.getChildren().add(pageButtonBox);
+    }
+
+    protected void setWorkspace(String pathToFXML) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainController.class.getResource(pathToFXML));
+        workspace.getChildren().setAll((Node) fxmlLoader.load());
     }
 }
